f53537b9c85c67773f9d872b39a3d0375645223f
creach-t <creach.t@gmail.com>
2024-08-30 16:36:06 +0200

    feat: :zap: Add map page + responsive

--- src/components/pages/ParcMap.tsx
+++ src/components/pages/ParcMap.tsx
@@ -2,11 +2,101 @@ import map from '../../assets/img/desktop/Rectangle-9-_1_.webp';
 
 function ParcMap() {
   return (
-    <div className="m-40">
-      <h2 className="uppercase text-5xl text-white mb-8">
+    <div className="mt-[104px] max-w-[1200px] mx-auto flex gap-4 flex-wrap justify-center mb-4">
+      <h2 className="ml-4 w-full uppercase text-6xl text-white mb-8">
         Plan<span className="text-redZombie"> du parc</span>
       </h2>
-      <img className="" src={map} alt="Plan des attractions de ZombieLand" />
+      <img
+        className="w-[500px] h-[500px]"
+        src={map}
+        alt="Plan des attractions de ZombieLand"
+      />
+      <ul className="ml-4 max-w-max grid grid-cols-2 lg:grid-cols-1 gap-1">
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>{' '}
+        <li>
+          <h3 className="text-xl text-redZombie uppercase">Zombie House</h3>
+          <p>
+            Visiter l&apos;antre des
+            zombies*****************************************
+          </p>
+        </li>
+      </ul>
     </div>
   );
 }
