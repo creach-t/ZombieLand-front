acd38847aa2733e2f3c4933bf8925e41edbb1c08
CedricMrt <cjyiphone@gmail.com>
2024-08-30 11:43:11 +0200

    fix: :lipstick: améliorations styles des liens

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -20,25 +20,28 @@ function NavBar() {
           </span>
         </a>
         <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="size-10 fill-redZombie"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <a href="/login" aria-label="login">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="hover:fill-red-700 size-10 fill-redZombie"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
 
-          <button
+          <a
+            href="/reservation"
             type="button"
-            className="text-white text-2xl bg-redZombie hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-dark-300 font-bold rounded-xl px-3 py-1 text-center"
+            className="text-white text-2xl bg-redZombie hover:bg-red-700 hover:outline-none hover:text-white focus:outline-none focus:ring-black font-bold rounded-xl px-3 py-1 text-center"
           >
             Réservation
-          </button>
+          </a>
           <button
             onClick={() => setIsNavOpen((prev) => !prev)}
             data-collapse-toggle="navbar-sticky"
