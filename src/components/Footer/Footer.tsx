511c7a1da19b19d292928b6e38069825dfdbbb8e
creach-t <creach.t@gmail.com>
2024-08-30 16:47:33 +0200

    fix: :zap: fixing color on link footer, fixing display of attraction list on map

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -18,32 +18,21 @@ function Footer() {
           <ul>
             <li>
               <Link
-                className="text-white hover:text-redZombie"
+                className="text-white hover:text-red-500"
                 href="/infos-pratiques"
               >
                 Infos pratiques
               </Link>
             </li>
             <li>
-              <Link
-                className="text-white hover:text-redZombie"
-                href="/attractions"
-              >
+              <Link className="text-white hover:text-red-500" to="/attractions">
                 Attractions
               </Link>
             </li>
             <li>
               <Link
-                className="text-white hover:text-redZombie"
-                href="/reserver"
-              >
-                Réservations
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white hover:text-redZombie"
-                href="/se-connecter"
+                className="text-white hover:text-red-500"
+                to="/se-connecter"
               >
                 Se connecter
               </Link>
@@ -65,7 +54,7 @@ function Footer() {
                 <path d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" />
               </svg>
               <a
-                className="text-white hover:text-redZombie"
+                className="text-white hover:text-red-500"
                 href="tel:+330666666666"
               >
                 +33 0666 666 666
@@ -81,7 +70,7 @@ function Footer() {
                 <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
                 <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
               </svg>
-              <Link className="text-white hover:text-redZombie" href="/contact">
+              <Link className="text-white hover:text-red-500" to="/contact">
                 Formulaire de contact
               </Link>
             </li>
@@ -96,7 +85,7 @@ function Footer() {
                 <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
               </svg>
               <a
-                className="text-white hover:text-redZombie"
+                className="text-white hover:text-red-500"
                 href="https://maps.app.goo.gl/yRq9KmqwEqojW5jS6"
               >
                 66 rue de l&apos;enfer <br /> 75000 Paris
