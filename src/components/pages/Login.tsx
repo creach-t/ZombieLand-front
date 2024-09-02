a905def814ae893147907bcfebcf289058280851
Sebastien MESA <mesasebastien@outlook.fr>
2024-09-02 14:49:55 +0200

    Initial commit Login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,50 @@
 function Login() {
   return (
-    <div className="m-40">
-      <h1>Login</h1>
+    <div className="w-4/5 m-auto">
+      <h2 className="text-7xl uppercase text-center mb-16">
+        Log<em className="text-redZombie">in</em>
+      </h2>
+      <form action="#">
+        <div className="mb-6 flex flex-col">
+          <label htmlFor="mail" className="text-3xl leading-loose">
+            E-mail
+          </label>
+          <input
+            type="text"
+            id="mail"
+            name="mail"
+            placeholder="Entrez votre E-mail"
+            className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+          />
+        </div>
+        <div className="mb-10 flex flex-col">
+          <label htmlFor="mail" className="text-3xl leading-loose">
+            Mot de passe
+          </label>
+          <input
+            type="text"
+            id="mail"
+            name="mail"
+            placeholder="Entrez votre mot de passe"
+            className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+          />
+          <a
+            href="#"
+            className="text-redZombie text-2xl text-right underline cursor-pointer"
+          >
+            Mot de passe oublié ?
+          </a>
+        </div>
+        <p className="bg-redZombie rounded-xl p-2 mb-2 hidden">
+          Nom d'utilisateur ou mot de passe non reconnu
+        </p>
+        <button
+          type="submit"
+          className="w-full mb-6 bg-greenZombie text-black text-3xl border-white border-2 rounded-xl md:max-w-xs self-center"
+        >
+          Me connecter
+        </button>
+      </form>
     </div>
   );
 }
