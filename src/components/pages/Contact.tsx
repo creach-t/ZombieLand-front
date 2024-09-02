c9df7375c20deb19e05655095f29b2c106f04a8d
Sebastien MESA <mesasebastien@outlook.fr>
2024-09-02 13:12:00 +0200

    feat: integration of contact page done

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -3,7 +3,7 @@ import contactImg from '../../assets/img/desktop/Zombie-contact.webp';
 function Contact() {
   return (
     <div className="main-container mt-40">
-      <div className="px-10">
+      <div className="px-20">
         <h2 className="text-6xl text-center md:text-left uppercase mb-6">
           Nous <em className="text-redZombie">contacter</em>
         </h2>
@@ -13,38 +13,43 @@ function Contact() {
           le fassent !{' '}
         </p>
       </div>
-      <div className="md:grid md:grid-cols-2 md:items-center">
+      <div className="md:grid md:grid-cols-2 md:items-center md:px-20">
         <img
           src={contactImg}
           alt="zombie au téléphone"
           className="my-8 mx-auto w-4/5 col-start-2"
         />
-        <form action="#" className="m-8 col-start-1 row-start-1">
-          <div className="mb-6">
-            <label htmlFor="firstname" className="text-3xl leading-loose">
-              Prénom
-            </label>
-            <input
-              type="text"
-              id="firstname"
-              name="firstname"
-              placeholder="Entrez votre prénom"
-              className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
-            />
+        <form
+          action="#"
+          className="m-8 col-start-1 row-start-1 md:flex md:flex-col"
+        >
+          <div className="md:grid md:grid-cols-2 md:gap-6">
+            <div className="mb-6 flex flex-col">
+              <label htmlFor="firstname" className="text-3xl leading-loose">
+                Prénom
+              </label>
+              <input
+                type="text"
+                id="firstname"
+                name="firstname"
+                placeholder="Entrez votre prénom"
+                className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+              />
+            </div>
+            <div className="mb-6 flex flex-col">
+              <label htmlFor="lastname" className="text-3xl leading-loose">
+                Nom
+              </label>
+              <input
+                type="text"
+                id="lastname"
+                name="lastname"
+                placeholder="Entrez votre nom"
+                className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+              />
+            </div>
           </div>
-          <div className="mb-6">
-            <label htmlFor="lastname" className="text-3xl leading-loose">
-              Nom
-            </label>
-            <input
-              type="text"
-              id="lastname"
-              name="lastname"
-              placeholder="Entrez votre nom"
-              className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
-            />
-          </div>
-          <div className="mb-6">
+          <div className="mb-6 flex flex-col">
             <label htmlFor="mail" className="text-3xl leading-loose">
               E-mail
             </label>
@@ -56,7 +61,7 @@ function Contact() {
               className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
             />
           </div>
-          <div className="mb-6">
+          <div className="mb-6 flex flex-col">
             <label htmlFor="message" className="text-3xl leading-loose">
               Message
             </label>
@@ -64,7 +69,7 @@ function Contact() {
               placeholder="Votre message ici..."
               name="message"
               id="message"
-              className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center h-60 resize-none"
+              className="w-full text-3xl border-white border-2 rounded-xl p-2 h-60 resize-none"
             />
           </div>
           <div className="mb-6 flex items-center">
@@ -81,7 +86,7 @@ function Contact() {
           </div>
           <button
             type="submit"
-            className="w-full bg-greenZombie text-black text-3xl border-white border-2 rounded-xl"
+            className="w-full bg-greenZombie text-black text-3xl border-white border-2 rounded-xl md:max-w-xs self-center"
           >
             Envoyer
           </button>
