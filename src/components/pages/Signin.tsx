9be01506fa380fac0054bc4e5ab68f4e3fa8b536
Kévin Martin <kevin.martin.albert@gmail.com>
2024-09-03 15:24:08 +0200

    feat: create a new user sends back JWT token and confirm message

--- src/components/pages/Signin.tsx
+++ src/components/pages/Signin.tsx
@@ -1,10 +1,42 @@
+import React, { useState } from "react";
+import api from '../api/api';
+
 function Signin() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await api.post('/signin', {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        password,
+      });
+
+      console.log('User created successfully:', response.data);
+
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setPassword('');
+      setErrorMessage('');
+    } catch (error: any) {
+      if (error.response && error.response.data) {
+        setErrorMessage('il y a eu un souci pendant la création du compte');
+      }
+    }
+  };
   return (
     <div className="w-4/5 md:max-w-5xl mt-40 m-auto">
       <h2 className="text-6xl uppercase text-center md:text-left mb-12">
         Sign<em className="text-redZombie">In</em>
       </h2>
-      <form action="#" className="md:flex md:flex-col">
+      <form onSubmit={handleSubmit} action="#" className="md:flex md:flex-col">
         <div className="mb-6 flex flex-col">
           <label htmlFor="mail" className="text-3xl leading-loose">
             Prénom
@@ -15,6 +47,8 @@ function Signin() {
             name="first_name"
             placeholder="Entrez votre prénom"
             className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
         <div className="mb-6 flex flex-col">
@@ -27,6 +61,8 @@ function Signin() {
             name="last_name"
             placeholder="Entrez votre nom"
             className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
           />
         </div>
         <div className="mb-6 flex flex-col">
@@ -39,6 +75,8 @@ function Signin() {
             name="mail"
             placeholder="Entrez votre E-mail"
             className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-10 flex flex-col">
@@ -51,6 +89,8 @@ function Signin() {
             name="password"
             placeholder="Entrez votre mot de passe"
             className="w-full text-3xl border-white border-2 rounded-xl p-2 text-center"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <p className="bg-redZombie rounded-xl p-2 mb-2 invisible">
