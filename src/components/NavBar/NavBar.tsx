11fdea2ad6c9bffea14abe8ac7056d48c8ec2b66
CedricMrt <cjyiphone@gmail.com>
2024-09-04 10:33:13 +0200

    fix bug

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
--- src/components/pages/404.tsx
+++ src/components/pages/404.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
 import pageNotFoundImg from '../../assets/img/desktop/404page 1.webp';
 
 function NotFoundPage() {
   return (
-    <div className="my-36 flex items-center justify-center">
+    <div className="mt-36 flex flex-col items-center justify-between">
       <img
         src={pageNotFoundImg}
         alt="page 404 not found"
-        className="w-full h-full max-w-5xl"
+        className="w-[400px]"
       />
     </div>
   );
