8ba6017b6f9335a17260e829b7126a1440983e74
Sebastien MESA <mesasebastien@outlook.fr>
2024-09-11 16:59:39 +0200

    fix : fixed front routein mymessages

--- src/components/pages/MyMessages.tsx
+++ src/components/pages/MyMessages.tsx
@@ -22,7 +22,7 @@ function MyMessages() {
         </h1>
         <div className="flex gap-4">
           <Link
-            to={`/mon-compte/${user?.user_id}`}
+            to={`/mon-compte`}
             className="text-3xl text-white border-white border-2 rounded-xl px-8 py-2 text-center mb-10"
           >
             Mes <em className="text-redZombie ">Informations</em>
