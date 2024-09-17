ffbc433b637eec05d7f541cde7d30cc99d4ae391
Kévin Martin <kevin.martin.albert@gmail.com>
2024-09-17 13:17:13 +0200

    fix: footer login link and create account link if user is not logged in, my account link if user is logged in

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,17 +2,21 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import { HashLink } from 'react-router-hash-link';
+import { useUser } from '../../context/UserContext';
+
 function Footer() {
+  const { user } = useUser();
+
   useEffect(() => {
     const links = document.querySelectorAll('a');
 
-    // Ajouter l'événement onClick à chaque lien pour défiler vers le haut
     links.forEach((link) => {
       link.addEventListener('click', () => {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
       });
     });
   }, []);
+
   return (
     <footer className="w-screen  text-white">
       <div className="flex flex-wrap gap-2 justify-around p-4 pb-0">
@@ -42,13 +46,34 @@ function Footer() {
               </Link>
             </li>
             <li>
-              <Link
-                className="text-white hover:text-red-500"
-                to="/se-connecter"
-              >
-                Se connecter
-              </Link>
+              {user ? (
+                <Link
+                  className="text-white hover:text-red-500"
+                  to="/mon-compte"
+                >
+                  Mon compte
+                </Link>
+              ) : (
+                <Link
+                  className="text-white hover:text-red-500"
+                  to="/se-connecter"
+                >
+                  Se connecter
+                </Link>
+              )}
             </li>
+            {!user ? (
+              <li>
+                <Link
+                  className="text-white hover:text-red-500"
+                  to="/inscription"
+                >
+                  Créer un compte
+                </Link>
+              </li>
+            ) : (
+              ""
+            )}
           </ul>
         </div>
         <div className="w-[150px]">
@@ -56,6 +81,7 @@ function Footer() {
             Nous <span className="text-redZombie">Contacter</span>
           </h3>
           <ul>
+            {/* Contact links */}
             <li className="flex gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -72,6 +98,7 @@ function Footer() {
                 +33 0666 666 666
               </a>
             </li>
+            {/* Address and contact form */}
             <li className="flex gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -110,6 +137,7 @@ function Footer() {
             Suivez-<span className="text-redZombie">nous</span>
           </h3>
           <ul className="flex gap-2">
+            {/* Social media links */}
             <li>
               <a
                 href="http://www.facebook.com/"
@@ -124,10 +152,11 @@ function Footer() {
                   viewBox="0 0 64 64"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M59.5 1H4.5C2.5 1 1 2.6 1 4.5V59.5C1 61.5 2.6 63 4.5 63H34.1V38.9H26.1V29.6H34.1V22.7C34.1 14.7 38.9 10.3 46.1 10.3C48.5 10.3 50.9 10.4 53.3 10.7V19H48.5C44.7 19 43.9 20.8 43.9 23.5V29.4H53L51.7 38.8H43.7V62.6H59.5C61.5 62.6 63 61.1 63 59.1V4.5C62.9 2.5 61.3 1 59.5 1Z" />
+                  <path d="M59.5 1H4.5C2.5 1 1 2.6 1 4.5V59.5C1 61.5 2.6 63 4.5 63H34.1V38.9H26.1V29.6H34.1V22.7C34.1 14.7 38.9 10.3 46.1 10.3C48.5 10.3 50.9 10.4 53.3 10.7V19H48.5C44.7 19 43.9 20.8 43.9 23.6V29.6H52.8L51.7 38.9H43.9V63H59.5C61.5 63 63 61.5 63 59.5V4.5C63 2.5 61.5 1 59.5 1Z" />
                 </svg>
               </a>
             </li>
+            {/* Instagram link */}
             <li>
               <a
                 href="http://www.instagram.com/"
@@ -142,36 +171,12 @@ function Footer() {
                   viewBox="0 0 64 64"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M32.1 43.6004C38.5065 43.6004 43.7 38.4069 43.7 32.0004C43.7 25.5939 38.5065 20.4004 32.1 20.4004C25.6935 20.4004 20.5 25.5939 20.5 32.0004C20.5 38.4069 25.6935 43.6004 32.1 43.6004Z" />
-                  <path d="M44.7 1H19.3C9.2 1 1 9.2 1 19.3V44.5C1 54.8 9.2 63 19.3 63H44.5C54.8 63 63 54.8 63 44.7V19.3C63 9.2 54.8 1 44.7 1ZM32.1 47.2C23.6 47.2 16.9 40.3 16.9 32C16.9 23.7 23.7 16.8 32.1 16.8C40.4 16.8 47.2 23.7 47.2 32C47.2 40.3 40.5 47.2 32.1 47.2ZM53.1 18.2C52.1 19.3 50.6 19.9 48.9 19.9C47.4 19.9 45.9 19.3 44.7 18.2C43.6 17.1 43 15.7 43 14C43 12.3 43.6 11 44.7 9.8C45.8 8.6 47.2 8 48.9 8C50.4 8 52 8.6 53.1 9.7C54.1 11 54.8 12.5 54.8 14.1C54.7 15.7 54.1 17.1 53.1 18.2Z" />
-                  <path d="M49.0016 11.5996C47.7016 11.5996 46.6016 12.6996 46.6016 13.9996C46.6016 15.2996 47.7016 16.3996 49.0016 16.3996C50.3016 16.3996 51.4016 15.2996 51.4016 13.9996C51.4016 12.6996 50.4016 11.5996 49.0016 11.5996Z" />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://www.tiktok.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <svg
-                  className="hover:fill-white"
-                  fill="#C90000"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 64 64"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M55.619 1H8.38095C4.3111 1 1 4.3111 1 8.38095V55.619C1 59.6889 4.3111 63 8.38095 63H55.619C59.6889 63 63 59.6889 63 55.619V8.38095C63 4.3111 59.6889 1 55.619 1ZM49.7231 28.0482C49.3846 28.0811 49.0447 28.0984 48.7046 28.0999C46.8681 28.1002 45.0604 27.6431 43.4448 26.7699C41.8291 25.8968 40.4563 24.635 39.4503 23.0986V40.1294C39.4503 42.6189 38.7121 45.0526 37.329 47.1226C35.9458 49.1926 33.9799 50.806 31.6799 51.7587C29.3798 52.7114 26.8489 52.9607 24.4072 52.475C21.9654 51.9893 19.7226 50.7905 17.9622 49.0301C16.2018 47.2697 15.0029 45.0268 14.5172 42.5851C14.0316 40.1433 14.2808 37.6124 15.2335 35.3124C16.1863 33.0123 17.7996 31.0464 19.8696 29.6633C21.9396 28.2801 24.3733 27.5419 26.8629 27.5419C27.1256 27.5419 27.3825 27.5655 27.6408 27.5818V33.7847C27.3825 33.7537 27.1286 33.7065 26.8629 33.7065C25.159 33.7065 23.5249 34.3833 22.3201 35.5881C21.1153 36.7929 20.4385 38.427 20.4385 40.1309C20.4385 41.8347 21.1153 43.4688 22.3201 44.6736C23.5249 45.8784 25.159 46.5552 26.8629 46.5552C30.4116 46.5552 33.5456 43.7593 33.5456 40.2106L33.6076 11.2861H39.5419C39.813 13.8645 40.9818 16.2656 42.844 18.0696C44.7062 19.8735 47.1433 20.9654 49.729 21.1544V28.0482" />
+                  <path d="M44.9 4H19.1C10.5 4 4 10.6 4 19.1V44.9C4 53.5 10.6 60 19.1 60H44.9C53.5 60 60 53.4 60 44.9V19.1C60 10.5 53.4 4 44.9 4ZM32 46.9C25.5 46.9 20.1 41.5 20.1 35C20.1 28.5 25.5 23.1 32 23.1C38.5 23.1 43.9 28.5 43.9 35C43.9 41.5 38.5 46.9 32 46.9ZM50.9 19.6C50.9 21.6 49.3 23.1 47.3 23.1C45.3 23.1 43.8 21.6 43.8 19.6C43.8 17.6 45.3 16.1 47.3 16.1C49.3 16.1 50.9 17.6 50.9 19.6ZM48.7 44.9C48.7 50.6 44.7 54.7 39.1 54.7H24.9C19.3 54.7 15.3 50.6 15.3 44.9V24.9C15.3 19.3 19.3 15.3 24.9 15.3H39.1C44.7 15.3 48.7 19.3 48.7 24.9V44.9Z" />
                 </svg>
               </a>
             </li>
           </ul>
         </div>
-      </div>
-      <div className="w-full text-center text-sm mt-2">
-        <p>Mentions légales</p>
-        <p>&copy; 2024 ZombieLand - Tout droits reservés</p>
       </div>
     </footer>
   );
