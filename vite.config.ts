2c1f62bd23327d517d9f7164b03d036b2f5dc934
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-04-02 18:36:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
   plugins: [
     react(),
-    Inspect({
-      build: true,
-      outputDir: '.vite-inspect',
-    }),
   ],
   build: {
     target: 'esnext', // Ou 'es2022' si 'esnext' pose des problèmes
